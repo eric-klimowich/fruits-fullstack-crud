50d2b00e7cb1a8939a313dc0e8fddc3b4079d591
eric-klimowich <eklimowich@hotmail.com>
2022-06-07 11:15:13 -0400

    Connected listener to PORT, finished initial server.js

--- server.js
+++ server.js
@@ -62,3 +62,10 @@ app.use(morgan("tiny")); //logging
 app.use(methodOverride("_method")); // override for put and delete requests from forms
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
+
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
